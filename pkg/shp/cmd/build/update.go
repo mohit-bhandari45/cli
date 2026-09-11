9c2a502bdd11deb8b83d7ace1a8d839e2e1ff57b
mohit-bhandari45 <mohitbhandari852@gmail.com>
2026-09-11 12:36:20 +0530

    feat(build): implement Run method for shp build update command
    Signed-off-by: mohit-bhandari45 <mohitbhandari852@gmail.com>

--- pkg/shp/cmd/build/update.go
+++ pkg/shp/cmd/build/update.go
@@ -6,6 +6,8 @@ import (
 	buildv1beta1 "github.com/shipwright-io/build/pkg/apis/build/v1beta1"
 	"github.com/spf13/cobra"
 
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 
 	"github.com/shipwright-io/cli/pkg/shp/cmd/runner"
@@ -57,4 +59,102 @@ func (c *UpdateCommand) Validate() error {
 		return fmt.Errorf("name must be provided")
 	}
 	return nil
+}
+
+// Run executes the update of an existing Build instance
+func (c *UpdateCommand) Run(params *params.Params, ioStreams *genericclioptions.IOStreams) error {
+	clientset, err := params.ShipwrightClientSet()
+	if err != nil {
+		return err
+	}
+
+	ns := params.Namespace()
+	build, err := clientset.ShipwrightV1beta1().Builds(ns).Get(c.cmd.Context(), c.name, metav1.GetOptions{})
+	if err != nil {
+		if k8serrors.IsNotFound(err) {
+			fmt.Fprintf(ioStreams.Out, "Build '%s' not found in namespace '%s'.\n", c.name, ns)
+			return nil
+		}
+		return err
+	}
+
+	// Update Git Source URL if flag was explicitly provided
+	if c.cmd.Flags().Changed(flags.SourceGitURLFlag) && c.buildSpec.Source != nil && c.buildSpec.Source.Git != nil {
+		if build.Spec.Source == nil {
+			build.Spec.Source = &buildv1beta1.Source{}
+		}
+		if build.Spec.Source.Git == nil {
+			build.Spec.Source.Git = &buildv1beta1.Git{}
+		}
+		build.Spec.Source.Git.URL = c.buildSpec.Source.Git.URL
+		build.Spec.Source.Type = buildv1beta1.GitType
+	}
+
+	// Update Git Revision if flag was explicitly provided
+	if c.cmd.Flags().Changed(flags.SourceGitRevisionFlag) && c.buildSpec.Source != nil && c.buildSpec.Source.Git != nil {
+		if build.Spec.Source == nil {
+			build.Spec.Source = &buildv1beta1.Source{}
+		}
+		if build.Spec.Source.Git == nil {
+			build.Spec.Source.Git = &buildv1beta1.Git{}
+		}
+		build.Spec.Source.Git.Revision = c.buildSpec.Source.Git.Revision
+	}
+
+	// Update ContextDir if flag was explicitly provided
+	if c.cmd.Flags().Changed(flags.SourceContextDirFlag) && c.buildSpec.Source != nil {
+		if build.Spec.Source == nil {
+			build.Spec.Source = &buildv1beta1.Source{}
+		}
+		build.Spec.Source.ContextDir = c.buildSpec.Source.ContextDir
+	}
+
+	// Update Strategy Name if flag was explicitly provided
+	if c.cmd.Flags().Changed(flags.StrategyNameFlag) {
+		build.Spec.Strategy.Name = c.buildSpec.Strategy.Name
+	}
+
+	// Update Strategy Kind if flag was explicitly provided
+	if c.cmd.Flags().Changed(flags.StrategyKindFlag) {
+		build.Spec.Strategy.Kind = c.buildSpec.Strategy.Kind
+	}
+
+	// Update Output Image if flag was explicitly provided
+	if c.cmd.Flags().Changed(flags.OutputImageFlag) {
+		build.Spec.Output.Image = c.buildSpec.Output.Image
+	}
+
+	// Update dockerfile param if changed
+	if c.dockerfile != nil && *c.dockerfile != "" && c.cmd.Flags().Changed(flags.DockerfileFlag) {
+		updateOrAppendParam(&build.Spec.ParamValues, "dockerfile", *c.dockerfile)
+	}
+
+	// Update builder-image param if changed
+	if c.builderImage != nil && *c.builderImage != "" && c.cmd.Flags().Changed(flags.BuilderImageFlag) {
+		updateOrAppendParam(&build.Spec.ParamValues, "builder-image", *c.builderImage)
+	}
+
+	flags.SanitizeBuildSpec(&build.Spec)
+
+	if _, err := clientset.ShipwrightV1beta1().Builds(ns).Update(c.cmd.Context(), build, metav1.UpdateOptions{}); err != nil {
+		return err
+	}
+
+	fmt.Fprintf(ioStreams.Out, "Updated build %q\n", c.name)
+	return nil
+}
+
+func updateOrAppendParam(paramValues *[]buildv1beta1.ParamValue, name string, val string) {
+	for i, p := range *paramValues {
+		if p.Name == name {
+			v := val
+			(*paramValues)[i].SingleValue = &buildv1beta1.SingleValue{Value: &v}
+			return
+		}
+	}
+	v := val
+	*paramValues = append(*paramValues, buildv1beta1.ParamValue{
+		Name: name,
+		SingleValue: &buildv1beta1.SingleValue{Value: &v},
+	})
 }
